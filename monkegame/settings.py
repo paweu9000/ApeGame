696a3e802d5186e8752bcc1c66d2a1488856c750
paweu9000 <99587672+paweu9000@users.noreply.github.com>
2022-05-13 19:19:54 +0200

    Update settings.py

--- monkegame/settings.py
+++ monkegame/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=yhh8bg($-ns3im(z3_u7_k8(k58ur$v%c9wv3_(zy&%cvq6*w'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
